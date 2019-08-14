64c520819a0c18d92abb8cbbbf2d6ff7e5a33bbb
Konstantin@jBoxers <roughnight2@gmail.com>
2019-08-14 16:35:55 +0300

    Added dynamic burger styling - building - removing and adding ingredients

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React, {Fragment} from 'react';
 
+import styles from '../Layout/Layout.module.css'
+
 const layout = (props) => (
  <Fragment>
  <div>Toolbar, SideDrawer, Backdroup</div>
- <main>
+ <main className={styles.Content}>
      {props.children}
  </main>
  </Fragment>
